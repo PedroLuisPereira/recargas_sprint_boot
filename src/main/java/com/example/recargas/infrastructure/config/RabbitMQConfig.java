7e3cf4d69a283486ea35ada6cbf260bc58e7684d
Pedro Luis Pereira Atencio <pedro.pereira@ceiba.com.co>
2023-09-11 21:09:53 -0500

    ok

--- src/main/java/com/example/recargas/infrastructure/config/RabbitMQConfig.java
+++ src/main/java/com/example/recargas/infrastructure/config/RabbitMQConfig.java
@@ -5,41 +5,77 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
+import org.springframework.amqp.support.converter.MessageConverter;
 
 @Configuration
 public class RabbitMQConfig {
 
+    @Value("${rabbitmq.exchange.name}")
+    private String exchange;
+
     @Value("${rabbitmq.queue.name}")
     private String queue;
 
-    @Value("${rabbitmq.exchange.name}")
-    private String exchange;
+    @Value("${rabbitmq.queue.json.name}")
+    private String jsonQueue;
 
     @Value("${rabbitmq.routing.key}")
     private String routingKey;
 
+    @Value("${rabbitmq.routing.json.key}")
+    private String routingJsonKey;
+
     // spring bean for rabbitmq queue
     @Bean
-    public Queue queue(){
+    public Queue queue() {
         return new Queue(queue);
+    }
+
+    // spring bean for queue (store json messages)
+    @Bean
+    public Queue jsonQueue() {
+        return new Queue(jsonQueue);
     }
 
     // spring bean for rabbitmq exchange
     @Bean
-    public TopicExchange exchange(){
+    public TopicExchange exchange() {
         return new TopicExchange(exchange);
     }
 
     // binding between queue and exchange using routing key
     @Bean
-    public Binding binding(){
+    public Binding binding() {
         return BindingBuilder
                 .bind(queue())
                 .to(exchange())
                 .with(routingKey);
     }
 
-// Spring boot autoconfiguration provides following beans
+    // binding between json queue and exchange using routing key
+    @Bean
+    public Binding jsonBinding() {
+        return BindingBuilder
+                .bind(jsonQueue())
+                .to(exchange())
+                .with(routingJsonKey);
+    }
+
+    @Bean
+    public MessageConverter converter() {
+        return new Jackson2JsonMessageConverter();
+    }
+
+    @Bean
+    public AmqpTemplate amqpTemplate(ConnectionFactory connectionFactory) {
+        RabbitTemplate rabbitTemplate = new RabbitTemplate(connectionFactory);
+        rabbitTemplate.setMessageConverter(converter());
+        return rabbitTemplate;
+    }
+
     // ConnectionFactory
     // RabbitTemplate
     // RabbitAdmin
---
+++ src/main/java/com/example/recargas/infrastructure/input/MessageJsonController.java
@@ -0,0 +1,28 @@
+package com.example.recargas.infrastructure.input;
+
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.recargas.infrastructure.config.RabbitMQJsonProducer;
+import com.example.recargas.infrastructure.config.User;
+
+@RestController
+@RequestMapping("/api/v1")
+public class MessageJsonController {
+
+    private RabbitMQJsonProducer jsonProducer;
+
+    public MessageJsonController(RabbitMQJsonProducer jsonProducer) {
+        this.jsonProducer = jsonProducer;
+    }
+
+    @PostMapping("/publish")
+    public ResponseEntity<String> sendJsonMessage(@RequestBody User user){
+        jsonProducer.sendJsonMessage(user);
+        return ResponseEntity.ok("Json message sent to RabbitMQ ...");
+    }
+}
