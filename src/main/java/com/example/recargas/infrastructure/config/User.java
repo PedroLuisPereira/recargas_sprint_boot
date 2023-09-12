75b3599ed7f66dbb70b91c808300308f5fa960ac
Pedro Luis Pereira Atencio <pedro.pereira@ceiba.com.co>
2023-09-11 21:36:28 -0500

    ok

--- src/main/java/com/example/recargas/infrastructure/config/User.java
+++ src/main/java/com/example/recargas/infrastructure/config/User.java
@@ -1,9 +1,14 @@
 package com.example.recargas.infrastructure.config;
 
+import java.io.Serializable;
+
 import lombok.Data;
 
 @Data
-public class User {
+public class User implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
     private int id;
     private String firstName;
     private String lastName;
